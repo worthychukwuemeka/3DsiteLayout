6953a2d444184c7e092e78d4e4393442913b611f
worthychukwuemeka <worthy.chukwuemeka@stu.cu.edu.ng>
2022-12-25 01:32:05 +0100

    first commit

---
+++ instead/main.js
@@ -0,0 +1,109 @@
+import './style.css'
+
+import * as THREE from 'three';
+
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+const renderer = new THREE.WebGLRenderer({
+    canvas: document.querySelector('#bg'),
+});
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+camera.position.setZ(30);
+
+renderer.render( scene, camera);
+
+const geometry = new THREE.TorusGeometry( 10, 3, 16, 100 );
+const material = new THREE.MeshStandardMaterial( { color: 0xFB607F} );
+const torus = new THREE.Mesh( geometry, material );
+scene.add( torus );
+
+const pointLight = new THREE.PointLight(0xFB607F)
+pointLight.position.set(20, 20, 20)
+
+const ambientLight = new THREE.AmbientLight(0xfffffff);
+scene.add(pointLight, ambientLight)
+
+const lightHelper = new THREE.PointLightHelper(pointLight)
+const gridHelper = new THREE.GridHelper(200, 50);
+scene.add(lightHelper, gridHelper)
+scene.add(pointLight)
+
+const controls = new OrbitControls(camera, renderer.domElement);
+
+function addStar() {
+    const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+    const material = new THREE.MeshStandardMaterial({color: 0xE5ACB6})
+    const star = new THREE.Mesh(geometry, material);
+
+    const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread( 100 ));
+
+    star.position.set(x, y, z);
+    scene.add(star)
+}
+
+Array(200).fill().forEach(addStar)
+
+const spaceTexture = new THREE.TextureLoader().load( 'space.jpg' );
+scene.background = spaceTexture;
+
+const worthyTexture = new THREE.TextureLoader().load('image2.png');
+
+const worthy = new THREE.Mesh(
+    new THREE.BoxGeometry(3,3,3),
+    new THREE.MeshStandardMaterial({map: worthyTexture})
+);
+
+scene.add(worthy);
+
+const moonTexture = new THREE.TextureLoader().load('ground.jpg');
+const normalTexture = new THREE.TextureLoader().load('normal.jpg');
+const moon = new THREE.Mesh(
+    new THREE.SphereGeometry(3, 32, 32),
+    new THREE.MeshStandardMaterial({
+        map: moonTexture,
+        normalMap: normalTexture
+    })
+);
+
+scene.add(moon);
+
+moon.position.z = 30;
+moon.position.setX(-10);
+
+function moveCamera(){
+    const t = document.body.getBoundingClientRect().top;
+    moon.rotation.x += 0.05;
+    moon.rotation.y += 0.0075;
+    moon.rotation.z += 0.05;
+
+    worthy.rotation.y += 0.01;
+    worthy.rotation.z += 0.01;
+
+    camera.position.z = t * -0.01;
+    camera.position.x = t * -0.0002;
+    camera.rotation.y = t * -0.0002;
+}
+
+document.body.onscroll = moveCamera
+
+function animate() {
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+
+    torus.rotation.x += 0.01;
+    torus.rotation.y += 0.005;
+    torus.rotation.x += 0.01;
+
+    controls.update();
+
+    renderer.render(scene, camera);
+}
+
+animate()
